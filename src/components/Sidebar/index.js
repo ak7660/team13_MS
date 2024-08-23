f525583865b8f5119e7bb127a2f79eeadb46032c
HUSSAIN Wasif Latif <wasiflh@connect.hku.hk>
2024-08-23 17:58:04 +0800

    Minor Updates and Setups

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -48,7 +48,7 @@ function Sidebar(props) {
           borderRadius={sidebarRadius}
         >
           <SidebarContent routes={routes}
-        logoText={"PURITY UI DASHBOARD"}
+        logoText={"ZUBIN FOUNDATION"}
         display="none"
         sidebarVariant={sidebarVariant}
         />
