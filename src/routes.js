664fbe33549fd59d95beb1f4303731c9f043463e
RichieWilynnton <richiewillynton@gmail.com>
2024-08-23 21:47:20 +0800

    first commit

--- src/routes.js
+++ src/routes.js
@@ -17,6 +17,7 @@ import {
   RocketIcon,
   SupportIcon,
 } from "components/Icons/Icons";
+import Analytics from "views/Dashboard/Analytics";
 
 var dashRoutes = [
   {
@@ -78,9 +79,8 @@ var dashRoutes = [
         path: "/data-analytics-dashboard",  // data analytics dashboard
         name: "Data Analytics Dashboard",
         icon: <RocketIcon color="inherit" />,
-        secondaryNavbar: true,
-        component: SignUp,
-        layout: "/auth",
+        component: Analytics,
+        layout: "/admin",
       },
     ],
   },
